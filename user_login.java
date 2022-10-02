2fed51fbde7b94d4881d03b2e53ecce09c17ebcf
bmlcrsgunasekara <97513246+bmlcrsgunasekara@users.noreply.github.com>
2022-10-03 00:56:59 +0530

    Update user_login.java
    I updated user_login page

--- user_login.java
+++ user_login.java
@@ -21,7 +21,6 @@ public class userlogin extends HttpServlet {
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
